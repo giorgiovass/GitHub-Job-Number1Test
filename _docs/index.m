5b45efe588396eff800a1818614f707442f6a37b
giorgiovass <102387036+giorgiovass@users.noreply.github.com>
2023-08-10 10:23:53 -0400

    Create index.m

---
+++ _docs/index.m
@@ -0,0 +1,66 @@
+d---
+permalink: /index.html
+page_order: 1
+hide_toc: true
+hide_nav: true
+hidden: true
+layout: doc_guide
+custom_collection: home
+nav_title: "Documentation"
+description: "This is the Braze documentation site landing page. Here, you can find links to the user guide, developer guide, API guide, technology partners documentation, and help articles. You can also find quick links to helpful links and popular articles."
+
+doc_top_header: "Braze Documentation"
+doc_top_text: ""
+
+braze_learning: Technical terms and concepts explained through a mix of videos, lessons, and interactive exercises.
+
+doc_menu_list:
+  - name: User Guide
+    bg_color: '#3accdd'
+    image: /assets/img/new-icons/users.svg
+    link: /docs/user_guide/introduction/
+    description: "Learn how to use the Braze platform to foster a more impactful customer experience."
+  - name: Developer Guide
+    bg_color: '#f7918e'
+    image: /assets/img/new-icons/html-file.svg
+    link: /docs/developer_guide/home/
+    description: "Integrate and activate your app or site with the Braze SDK."
+  - name: Technology Partners
+    bg_color: '#ff9349'
+    image: /assets/img/new-icons/gears.svg
+    link: /docs/partners/home/
+    description: "Our Technology Partners can help you customize your Braze experience and customer relationships."
+  - name: API Guide
+    bg_color: '#ff9349'
+    image: /assets/img/new-icons/api.svg
+    link: /docs/api/home/
+    description: "Braze provides a high-performance REST API to allow you to track users, send messages, export data, and more."
+
+popular_topic_list:
+  - name: "Canvas Basics"
+    link: /docs/user_guide/engagement_tools/canvas/get_started/the_basics/
+  - name: Create a Campaign
+    link: /docs/user_guide/engagement_tools/campaigns/building_campaigns/creating_campaign/
+  - name: Personalized Messaging
+    link: /docs/user_guide/personalization_and_dynamic_content/overview/
+  - name: Braze API Dictionary
+    link: /docs/api/home/
+  - name: Custom Events and Attributes
+    link: /docs/user_guide/data_and_analytics/custom_data/
+  - name: User Import
+    link: /docs/user_guide/data_and_analytics/user_data_collection/user_import/
+  - name: Create a Catalog
+    link: /docs/user_guide/personalization_and_dynamic_content/catalogs
+  - name: Braze Currents
+    link: /docs/user_guide/data_and_analytics/braze_currents
+
+helpful_resources_list:
+  - name: Innovation Statement
+    link: /docs/innovation_statement/
+  - name: Support & Success Services Handbook
+    link: /docs/handbooks/
+  - name: Data Retention
+    link: /docs/api/data_retention/
+  - name: Data Protection Technical Assistance
+    link: /docs/dp-technical-assistance/
+---
